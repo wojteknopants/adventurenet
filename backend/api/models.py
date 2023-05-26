fd25782fe218fafe1d33098902b27030b316e30b
Wojciech <wojmat4@st.amu.edu.pl>
2023-05-26 17:25:17 +0200

    added permissions to UserProfile endpoints

--- backend/api/models.py
+++ backend/api/models.py
@@ -45,9 +45,9 @@ class UserProfile(models.Model):
     )
     name = models.CharField(max_length=20, null=True, blank=True)
     surname = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=20, null=True, blank=True)
-    bio = models.CharField(max_length=200, null=True, blank=True)
-    username = models.CharField(max_length=20, null=True)
+    country = models.CharField(max_length=20, null=True, blank=True, default="Poland")
+    bio = models.CharField(max_length=200, null=True, blank=True, default= "This is your example bio. Edit this however you want it.")
+    username = models.CharField(max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,19 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAdminUser
+from djoser.permissions import CurrentUserOrAdminOrReadOnly
 from .models import UserProfile, UserAccount
 from .serializers import UserProfileSerializer
 
 class UserProfileListCreateAPIView(ListCreateAPIView):
     """It returns list of UserProfiles when GET,
     it creates new object when POST"""
+    permission_classes = [IsAdminUser] #only admin can list info of all profiles at once, also only admin can create a new profile (not recommended, profile should be auto created only through registration)
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
 class UserProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     """It returns single object when GET, updates (writes on) object when POST or PATCH, and destroys when DELETE"""
+    permission_classes = [CurrentUserOrAdminOrReadOnly] #only current user or admin can edit profile info, everybody can read profile info
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = 'user__pk'
