cce388bbe69b1dc80f7bf4ea48aca2270fc07a33
Wojciech <wojmat4@st.amu.edu.pl>
2023-05-26 19:32:13 +0200

    added profiles/me/ functionality

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from .views import UserProfileListCreateAPIView, UserProfileRetrieveUpdateAPIVie
 
 urlpatterns = [
     path('profiles/', UserProfileListCreateAPIView.as_view(), name='userprofile_list_create'),
-    path('profiles/<int:user__pk>/', UserProfileRetrieveUpdateAPIView.as_view(), name='userprofile_retrieve_update'),
+    path('profiles/<str:user__pk>/', UserProfileRetrieveUpdateAPIView.as_view(), name='userprofile_retrieve_update'), #can use profiles/me/ to capture user__id from JWT token
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -12,8 +12,22 @@ class UserProfileListCreateAPIView(ListCreateAPIView):
     serializer_class = UserProfileSerializer
 
 class UserProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    """It returns single object when GET, updates (writes on) object when POST or PATCH, and destroys when DELETE"""
+    """It returns single object when GET, updates (writes on) object when POST or PATCH, and destroys when DELETE. Requires user.id as url parameter, e.g. profiles/1/.
+    Can be used as profiles/me/ to show profile of JWT Token owner """
+
+    def get_object(self): 
+        if self.kwargs['user__pk'] == 'me': #if instead profiles/{user.id}/ we would use profiles/me/ (to capture user.id from JWT token)
+            user = self.request.user
+            self.kwargs['user__pk'] = user.pk
+            return super().get_object()
+        else: 
+            return super().get_object()
+        
     permission_classes = [CurrentUserOrAdminOrReadOnly] #only current user or admin can edit profile info, everybody can read profile info
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = 'user__pk'
+
+    
+
+
