1587826fe93feaccab351d1cf4524fc461a37551
EgoFox <egotdp4@gmail.com>
2023-05-27 14:16:28 +0200

    add to previous commit

--- frontend/src/features/index.ts
+++ frontend/src/features/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth/authSlice";
+import profileReducer from "./profile/profileSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  profile: profileReducer,
 });
 
 export default rootReducer;
