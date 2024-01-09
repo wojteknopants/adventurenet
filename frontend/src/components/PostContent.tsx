3dc24ba101e8855cbd47333414c0099134a8b738
Ego <egotdp4@gmail.com>
2024-01-09 20:45:01 +0100

    Fix post adding and comments

--- frontend/src/components/PostContent.tsx
+++ frontend/src/components/PostContent.tsx
@@ -12,15 +12,7 @@ const PostContent = ({
   return (
     <div>
       <div className="rounded-xl overflow-hidden mt-2 max-h-[340px]">
-        {image ? (
-          <img src={`${image}`} alt="Error" />
-        ) : (
-          <img
-            className=" m-auto w-[300px] h-full "
-            style={{ filter: "invert(90%) grayscale(100%)" }}
-            src={postsPlaceholder}
-          />
-        )}
+        {image && <img src={`${image}`} alt="Error" />}
       </div>
       <p className="my-3 leading-5 text-[14px] text-mainGray">{content}</p>
     </div>
